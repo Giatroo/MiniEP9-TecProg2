6104418d2cbd4df89c9b9f3fe170327a14c4a4de
Lucas Paiolla <lucaspaiolla@gmail.com>
2022-06-14 15:15:54 -0300

    add tests for buttons on the main page

--- test_main.py
+++ test_main.py
@@ -11,6 +11,30 @@ class TestFundaments(unittest.TestCase):
         cls.driver = webdriver.Chrome()
         cls.driver.get("https://www.fundamentus.com.br/index.php")
 
+    def test_empresa_button(self):
+        """Tests if the button 'empresa' on the main page redirects the user to
+        the right page.
+        """
+        self.driver.find_element(
+            by="xpath", value="/html/body/div[1]/div[1]/div[1]/span/a[1]"
+        ).click()
+        self.assertEqual(
+            self.driver.current_url,
+            "https://www.fundamentus.com.br/buscaavancada.php",
+        )
+
+    def test_fii_button(self):
+        """Tests if the button 'fii' on the main page redirects the user to
+        the right page.
+        """
+        self.driver.find_element(
+            by="xpath", value="/html/body/div[1]/div[1]/div[1]/span/a[2]"
+        ).click()
+        self.assertEqual(
+            self.driver.current_url,
+            "https://www.fundamentus.com.br/fii_buscaavancada.php",
+        )
+
     @classmethod
     def tearDownClass(cls):
         cls.driver.quit()
